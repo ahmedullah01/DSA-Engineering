6943b21298e1d43b446f880c7d32b59fc68fd0a8
Ahmedullah <ahmedullah2095@gmail.com>
2026-02-27 08:07:52 +0500

    Add files via upload

---
+++ Hashing/Closed_LinearProbing.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+class Student Ope
+public:
+   in    t array[10]; // Correct way to declare a fixed-size array in C++
+
+    void insert(int number){
+        int index = xx`
+};
+
+int main() {
+    Student s1;   // object banaya
+
+    s1.name = "Ahmed";
+    s1.age = 20;
+
+    s1.display();  // function call
+
+    return 0;
+}
